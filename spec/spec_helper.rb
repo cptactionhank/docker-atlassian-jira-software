b835838a022050cbf593ec625b0cd522e0d772fe
Circle CI Automated Builder <circleci@circleci>
2015-12-21 20:40:28 +0100

    Updated acceptance tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,14 +7,7 @@ REGEX_WARN    = /WARNING|WARN/
 REGEX_ERROR   = /ERROR|ERR/
 REGEX_SEVERE  = /SEVERE|FATAL/
 REGEX_STARTUP = /Server startup in \d+ ms/
-REGEX_FILTER  = Regexp.compile (Regexp.union [
-  %r{org\.apache\.catalina\.loader\.WebappClassLoaderBase\.checkThreadLocalMapForLeaks\ The\ web\ application\ \[ROOT\]\ created\ a\ ThreadLocal\ with\ key\ of\ type\ \[.*\]\ \(value\ \[.*\]\)\ and\ a\ value\ of\ type\ \[.*\]\ \(value\ \[.*\]\)\ but\ failed\ to\ remove\ it\ when\ the\ web\ application\ was\ stopped\.\ Threads\ are\ going\ to\ be\ renewed\ over\ time\ to\ try\ and\ avoid\ a\ probable\ memory\ leak\.},
-  # For some reason when setting up the database the indexing path is not set
-  # yielding the following errors.
-  %r{\[c\.a\.jira\.upgrade\.ConsistencyCheckImpl\]\ Indexing\ is\ turned\ on,\ but\ index\ path\ \[null\]\ invalid\ \-\ disabling\ indexing},
-  %r{\[c\.a\.j\.issue\.index\.DefaultIndexManager\]\ File\ path\ not\ set\ \-\ not\ indexing},
-  %r{\[o\.e\.g\.b\.e\.i\.util\.concurrent\.RunnableTimedExecution\]\ Closing\ runnable\ for\ context\ NonValidatingOsgiBundleXmlApplicationContext\(bundle=com\.pyxis\.greenhopper\.jira,\ config=osgibundle:/META\-INF/spring/\*\.xml\)\ did\ not\ finish\ in\ 10000ms;\ consider\ taking\ a\ snapshot\ and\ then\ shutdown\ the\ VM\ in\ case\ the\ thread\ still\ hangs}
-])
+REGEX_FILTER  = Regexp.compile (Regexp.union [])
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |file| require file }
 
