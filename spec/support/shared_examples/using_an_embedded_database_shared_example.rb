eb9d61ca51926cead609f437359fdb40219b4b7d
Circle CI Automated Builder <circleci@circleci>
2016-02-07 15:43:49 +0100

    Updated CI configurations, acceptance tests, and support for proxy configuration

--- spec/support/shared_examples/using_an_embedded_database_shared_example.rb
+++ spec/support/shared_examples/using_an_embedded_database_shared_example.rb
@@ -2,10 +2,10 @@ require 'timeout'
 require 'spec_helper'
 
 shared_examples 'using an embedded database' do
-  before :all do
-    within 'form#jira-setup-database' do
-      choose 'jira-setup-database-field-database-internal'
-      click_button 'Next'
-    end
-  end
+	before :all do
+		within 'form#jira-setup-database' do
+			choose 'jira-setup-database-field-database-internal'
+			click_button 'Next'
+		end
+	end
 end
